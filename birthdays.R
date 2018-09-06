aa6410a9358af97ae3a80df08f57d96a3596f842
zafiroandrade <zafiroandrade@hotmail.com>
2018-09-06 16:21:18 -0700

    cambios

--- birthdays.R
+++ birthdays.R
@@ -2,10 +2,10 @@
 # What is your name?
 
 
-# When is your birthdate? Only Month and day. 
+# When is your birthdate? Only Month and day.
 # Format "Month/date". For example for date "January 9th", we write "01/09"
-my_name <- "WRITE YOUR FIRST NAME HERE"
-my_bd <- "DATE HERE IN FORMAT DD/MM"
+my_name <- "Zafiro"
+my_bd <- "08/31"
 
 
 # When done, save, commit, push and make your first pull request.
@@ -15,5 +15,3 @@ df <- data.frame("names" = c("Fernando", "Isabel") ,
                  "birthdate" = as.Date(c("01/09","05/29")
                                        , "%m/%d") )
 qplot(data = df, x = names, y = birthdate )
-
-
